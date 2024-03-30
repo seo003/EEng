14cb044a1a65e080b2986e1c2f89545c6545d9a6
JulGoo <yrruda94@gmail.com>
2024-03-30 23:11:47 +0900

    eeng

---
+++ src/main/java/kr/kro/eeng/EEngApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.eeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EEngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EEngApplication.class, args);
+	}
+
+}
