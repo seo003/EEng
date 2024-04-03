a9270b55e7e31b26311a910042d84c2828c4d552
JulGoo <yrruda94@gmail.com>
2024-04-03 23:41:22 +0900

    gyr_level_240403

--- src/main/java/kr/kro/eeng/levelTest/LevelTestC.java
+++ src/main/java/kr/kro/eeng/levelTest/LevelTestC.java
@@ -1,8 +1,12 @@
 package kr.kro.eeng.levelTest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LevelTestC {
-
+    @GetMapping("/level-test")
+    public String levelTest(){
+        return "levelTest";
+    }
 }
