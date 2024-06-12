6cb730f8727363ea14a063125a82352dd4944808
JulGoo <yrruda94@gmail.com>
2024-06-12 17:28:59 +0900

    gyr_240612

--- src/main/java/kr/kro/eeng/interceptor/LoginInterceptor.java
+++ src/main/java/kr/kro/eeng/interceptor/LoginInterceptor.java
@@ -10,11 +10,11 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override //요청이 컨트롤러로 전달 되기 전에 실행되는 로직 정의
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
-//        if (session.getAttribute("loginId") == null) {
-//            // 로그인하지 않은 사용자를 로그인 페이지로 리다이렉트
-//            response.sendRedirect("/login.do?error=loginRequired");
-//            return false; // 요청 처리를 계속하지 않음
-//        }
+        if (session.getAttribute("loginId") == null) {
+            // 로그인하지 않은 사용자를 로그인 페이지로 리다이렉트
+            response.sendRedirect("/login.do?error=loginRequired");
+            return false; // 요청 처리를 계속하지 않음
+        }
 
         return true; // 요청 처리 계속
     }
